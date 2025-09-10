06896473afcff0a333a18cd227bdd66084e2e21f
Scoofszlo <scoofszlo@gmail.com>
2025-09-10 21:18:05 +0800

    Fix issue

--- src/tikorgzo/core/extractor.py
+++ src/tikorgzo/core/extractor.py
@@ -34,9 +34,7 @@ class Extractor:
             raise MissingPlaywrightBrowserError()
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        if self.browser:
-            await self.browser.close()
-        await self.playwright.stop()
+        await asyncio.wait_for(self._cleanup(), timeout=5)
 
     async def process_video_links(self, videos: list[Video]) -> list[Video | BaseException]:
         tasks = [self._extract(video) for video in videos]
@@ -63,6 +61,12 @@ class Extractor:
                 URLParsingError,
             ) as e:
                 console.print(f"Skipping {video.video_id} due to: [red]{type(e).__name__}: {e}[/red]")
+                # Needs to re-raise so that the mainline script (main.py) will caught this exception
+                # thus, the program can filter tasks that are successful and not these failed tasks
+                # due to these exception
+                raise e
+            except asyncio.CancelledError as e:
+                console.print(f"Skipping {video.video_id} due to: [red]UserCancelledAction[/red]")
                 # Needs to re-raise so that the mainline script (main.py) will caught this exception
                 # thus, the program can filter tasks that are successful and not these failed tasks
                 # due to these exception
@@ -145,3 +149,8 @@ class Extractor:
                 response.raise_for_status()
                 total_size_bytes = int(response.headers.get('content-length', 0))
                 return total_size_bytes
+
+    async def _cleanup(self) -> None:
+        if self.browser:
+            await self.browser.close()
+        await self.playwright.stop()
