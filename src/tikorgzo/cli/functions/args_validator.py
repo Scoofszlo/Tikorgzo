bb95cf28eb4002a00f89865bf2e43a3048de0b40
Scoofszlo <scoofszlo@gmail.com>
2025-09-30 21:53:54 +0800

    Add validation to check if one of the placeholders have been used

--- src/tikorgzo/cli/functions/args_validator.py
+++ src/tikorgzo/cli/functions/args_validator.py
@@ -16,6 +16,7 @@ def validate_args(ah_param: ArgsHandler, args_param: Namespace):
 
     _show_cli_help()
     _raise_error_if_invalid_max_concurrent_downloads()
+    _raise_error_if_invalid_filename_string()
 
 
 def _show_cli_help():
@@ -28,4 +29,16 @@ def _raise_error_if_invalid_max_concurrent_downloads():
     if args.max_concurrent_downloads:
         if args.max_concurrent_downloads > 16 or args.max_concurrent_downloads < 1:
             console.print("[red]error[/red]: '[blue]--max-concurrent-downloads[/blue]' must be in the range of 1 to 16.")
+            sys.exit(1)
+
+
+def _raise_error_if_invalid_filename_string():
+    placeholders = {
+        "necessary": ["video_id"],
+        "optional": ["username"]
+    }
+
+    for placeholder in placeholders["necessary"]:
+        if placeholder not in args.filename_template:
+            console.print(f"[red]error[/red]: '[blue]--filename-template[/blue]' does not contain one of the needed placeholders: [green]{placeholders['necessary']}[/green]")
             sys.exit(1)
