3bac754ee833f03014b2dc419b8f55d6ed58fb57
Scoofszlo <scoofszlo@gmail.com>
2026-02-18 09:43:45 +0800

    Create two strategies based on client session

---
+++ src/tikorgzo/core/download_manager/strategies/aiohttp.py
@@ -0,0 +1,39 @@
+import aiofiles
+import aiohttp
+from rich.progress import Progress
+
+from tikorgzo.constants import DownloadStatus
+from tikorgzo.core.download_manager.constants import CHUNK_SIZE, STATUS_OK
+from tikorgzo.core.download_manager.strategies._base import BaseDownloadStrategy
+from tikorgzo.core.video.model import Video
+
+
+class AioHTTPDownloadStrategy(BaseDownloadStrategy):
+    """Downloads a video using an aiohttp session."""
+
+    def __init__(self, session: aiohttp.ClientSession) -> None:
+        self.session = session
+
+    async def download(self, video: Video, progress: Progress) -> None:
+        async with self.session.get(video.download_link) as response:
+            if response.status != STATUS_OK:
+                video.download_status = DownloadStatus.INTERRUPTED
+                raise aiohttp.ClientResponseError(
+                    request_info=response.request_info,
+                    history=response.history,
+                    status=response.status,
+                    message=f"Failed to download {video.video_id}: {response.status}",
+                    headers=response.headers,
+                )
+
+            total_size = video.file_size.get()
+            assert isinstance(total_size, float)
+
+            task = progress.add_task(str(video.video_id), total=total_size)
+            async with aiofiles.open(video.output_file_path, "wb") as output_file:
+                async for chunk in response.content.iter_chunked(CHUNK_SIZE):
+                    if chunk:
+                        await output_file.write(chunk)
+                        progress.update(task, advance=len(chunk))
+
+            video.download_status = DownloadStatus.COMPLETED
---
+++ src/tikorgzo/core/download_manager/strategies/requests.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+
+import requests
+from requests import HTTPError
+from rich.progress import Progress
+
+from tikorgzo.constants import DownloadStatus
+from tikorgzo.core.download_manager.constants import CHUNK_SIZE, STATUS_OK
+from tikorgzo.core.download_manager.strategies._base import BaseDownloadStrategy
+from tikorgzo.core.video.model import Video
+
+
+class RequestsDownloadStrategy(BaseDownloadStrategy):
+    """Downloads a video using a requests session."""
+
+    def __init__(self, session: requests.Session) -> None:
+        self.session = session
+
+    async def download(self, video: Video, progress: Progress) -> None:
+        try:
+            response = self.session.get(video.download_link, stream=True)
+
+            if response.status_code != STATUS_OK:
+                video.download_status = DownloadStatus.INTERRUPTED
+                msg = f"Failed to download {video.video_id}: {response.status_code}"
+                raise HTTPError(msg)  # noqa: TRY301
+
+            total_size = video.file_size.get()
+            assert isinstance(total_size, float)
+
+            task = progress.add_task(str(video.video_id), total=total_size)
+            with Path.open(video.output_file_path, "wb", encoding=None) as output_file:  # pylint: disable=unspecified-encoding
+                for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+                    if chunk:
+                        output_file.write(chunk)
+                        progress.update(task, advance=len(chunk))
+
+            video.download_status = DownloadStatus.COMPLETED
+        except (HTTPError, Exception):
+            video.download_status = DownloadStatus.INTERRUPTED
+            raise
