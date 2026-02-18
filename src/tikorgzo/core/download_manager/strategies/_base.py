c6fa34657eee40a0ebfa4c8e30355fd0949b3acc
Scoofszlo <scoofszlo@gmail.com>
2026-02-18 19:02:39 +0800

    Fix lint issue introduced by #49 (#50)

--- src/tikorgzo/core/download_manager/strategies/_base.py
+++ src/tikorgzo/core/download_manager/strategies/_base.py
@@ -11,3 +11,9 @@ class BaseDownloadStrategy(ABC):
     @abstractmethod
     async def download(self, video: Video, progress: Progress) -> None:
         """Download the video and update the progress display."""
+
+    @staticmethod
+    def _print_failed_status(video: Video, status_code: int, progress: Progress) -> None:
+        """Print a message when a download fails due to a non-OK status code."""
+        msg = f"[gray50]Failed to download {video.video_id} due to[/gray50]: [orange1]{status_code} status code[/orange1]"
+        progress.console.print(msg)
