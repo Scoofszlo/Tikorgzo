8126b0249deede51c37f2b3e66eb06994a40d0b8
Scoofszlo <scoofszlo@gmail.com>
2026-02-18 20:19:04 +0800

    Convert the old class into helper functions

---
+++ src/tikorgzo/core/video/constants.py
@@ -0,0 +1,4 @@
+USERNAME_REGEX = r"\/@([\w\.\-]+)\/video\/\d+"
+NORMAL_TIKTOK_VIDEO_LINK_REGEX = r"(https?://)?(www\.)?tiktok\.com/@[\w\.\-]+/video/\d+(\?.*)?$"
+VT_TIKTOK_VIDEO_LINK_REGEX = r"(https?://)?vt\.tiktok\.com/"
+IDEAL_BINARY_NUM_LEN = 64
